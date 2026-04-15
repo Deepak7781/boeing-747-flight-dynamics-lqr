4b46724892eb48b77ded70dcac65f0915e15efe9
Deepak7781 <deepak21082005@gmail.com>
2026-04-15 20:04:22 +0530

    eigen value analysis

--- src/main.m
+++ src/main.m
@@ -1,2 +1,12 @@
 ac = boeing747Params();
 [lonSS, latSS] = stateSpaceModel(ac);
+
+Alon = lonSS.A;
+
+% Compute the eigenvalues of the state matrix Alon
+lonEigen = eig(Alon);
+
+Alat = latSS.A;
+
+latEigen = eig(Alat);
+
